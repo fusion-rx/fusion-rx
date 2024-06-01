0ed081cdb26a030ca406da2e14a0ce629942dc40
Alexander Porrello <alexander.porrello@kapsch.net>
2024-05-31 20:56:46 -0500

    Removes stale code.

--- tools/.legacy/guard.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-import { Req, Res } from './express';
-
-export declare interface Guard {
-    canActivate(
-        req: Req,
-        res: Res
-    ): boolean | Observable<boolean> | Promise<boolean>;
-}
